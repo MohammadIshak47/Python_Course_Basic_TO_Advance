48522cf9536f7f293f20eff27432e8c78503784a
Mohammad <ishak1998bd@gmail.com>
2022-12-29 09:11:37 +0530

    updated more info

---
+++ Part_01/basic_concepts.py
@@ -0,0 +1,46 @@
+"""
+## Every language has there grammatical rules .This rules are called syntax
+## In computer data is nothing like binary numbers like 0 ,1 
+### Data/values can be stored in temporary storage spaces called variables
+### Variable is name associated and it is also address associate
+
+## Example : 
+num1 = 10 ## Here num1 is variable and 10 is value which is stored in our variable 
+num2 = 20 
+print(num1+num2)
+
+## Decission making variable ... if else elif
+
+### Looping statements are used to repeat a task in multiple times.
+
+# Function is a block of code which performs a specific task. 
+# Like Deposit_ Deposit is a function to deposit money,
+# Withdraw __ it is a function to withdraw money
+# Balance __ it is also a function to check money 
+
+### class is a template/bluprint for real world entities 
+
+## class has two behavior exist . 1_ is attribute/properties like mobile phone price , brand, model
+color,battery life etc and 2_ is behavior/actions/methods like make a call , see videos,
+send emails , calculate a number etc.
+
+### Objects are specific instances of a class. Like phone is a class and it's specific instances
+/object would be samsung, nokia , apple , oop etc. these are brand of phone clas which is object.
+
+Example :  Samsung is an object  . it has different colors/battery life/price and using samsung 
+can make a call , play games , watch videos . all the brand of mobile , it's attributes/property
+and its methods/actions/behavior is comes together in one umbrella and it is called class.
+
+### Algorithms__ step by step approach to solve a problem is known as algorithm
+like <input> ...<steps to followed> ...<output>
+
+how to make a glass of lemon juice?
+step_01 : check if you have all the ingredient/elements
+step_02 : Take lemon and cut into halves
+step_03 : Squeeze lemon into a glass of water
+step_04 : Add sugar and stir it well
+step_05 : serve it cold with ice cubes
+
+
+
+"""
