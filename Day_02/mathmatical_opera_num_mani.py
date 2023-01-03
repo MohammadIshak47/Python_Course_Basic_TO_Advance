00cad88420a6c9b4de978e18aa71e8d0f3d4178b
Mohammad <ishak1998bd@gmail.com>
2023-01-03 18:53:44 +0530

    updated final project code

--- Day_02/mathmatical_opera_num_mani.py
+++ Day_02/mathmatical_opera_num_mani.py
@@ -112,19 +112,21 @@ print(f"You have {remaining_days} days, {remaining_weeks} weeks, and {remaining_
 
 #### Final project of Day_02:
 
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill ? $ "))
+tip= int(input("What percentage would you like to give? 10,12,15? "))
+people = int(input("How many people to split the bill ? "))
+
+bill_with_tip = tip/100*bill+bill
+
+bill_per_person = bill_with_tip/people
+
+print("Each person should pay bill : ",bill_per_person)
+
 
 '''
 
-print("Welcome to the tip calculator")
-total_bill = float((input("What was the total bill ? ")))
-given_percentage = int(input("What percentage would you like to give? 10,12,15? "))
-people_split = int(input("How many people to split the bill ? "))
 
-total_bill_per_person = int(total_bill)/people_split
-given_percentage_per_person = given_percentage/100
-
-each_person_pay = total_bill_per_person*given_percentage_per_person
-print("Each person should pay bill : ",float(each_person_pay))
 
 
 
