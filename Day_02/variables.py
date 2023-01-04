f46b060fa9bef39904f9819baaf2c10ec237b38d
Mohammad <ishak1998bd@gmail.com>
2023-01-04 13:29:19 +0530

    updated typecasting file

--- Day_02/variables.py
+++ Day_02/variables.py
@@ -70,6 +70,20 @@ Output:
 
 {'name': 'Sakshi', 'age': 20, 'canVote': True}
 
+
+Operators
+
+Python has different types of operators for different operations. To create a calculator we require arithmetic operators.
+Arithmetic operators
+Operator	Operator Name	Example
++	Addition	15+7
+-	Subtraction	15-7
+*	Multiplication	5*7
+**	Exponential	5**3
+/	Division	5/3
+%	Modulus	15%7
+//	Floor Division	15//7
+
 ## problem _01:Create a calculator capable of performing addition, subtraction, 
 # multiplication and division operations on two numbers. Your program should format
 #  the output in a readable manner!
