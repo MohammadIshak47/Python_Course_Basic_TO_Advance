007f7604ad8dc967f3ac062db57f48c7f79a04fb
Mohammad <ishak1998bd@gmail.com>
2023-01-10 07:08:30 +0530

    added a password generator project

---
+++ Day_05/password_generator_project.py
@@ -0,0 +1,18 @@
+#### Password generator 
+
+
+import random
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'
+'t','u','v','w','x','y','z','A', 'B', 'C', 'D', 'E' ,'F', 'G', 'H', 'I', 'J', 'K', 'L',
+ 'M', 'N' ,'O' ,'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+symbols = ['!','@','#','$','%','&','*',(','),'+']
+
+print("Welcome to my PyPassword Generator")
+nr_letters = int(input("How many letters would you like to in your password?\n"))
+nr_symbols = int(input("How many symbols would you like to?\n"))
+nr_numbers = int(input("How many numbers would you like to?\n"))
+
