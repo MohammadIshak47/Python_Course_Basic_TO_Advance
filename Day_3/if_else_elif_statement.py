7df6ba3917a06504ef9ec30e2d5673c23b5842dc
Mohammad <ishak1998bd@gmail.com>
2023-01-19 11:18:13 +0530

    added more info

--- Day_3/if_else_elif_statement.py
+++ Day_3/if_else_elif_statement.py
@@ -162,10 +162,44 @@ if number%2==0:
     print(number," is even number .")
 else:
     print(number,"is odd number.") 
+
+
+
+Excersice 2: Good Morning Sir
+
+Create a python program capable of greeting you with Good Morning, Good Afternoon 
+and Good Evening. Your program should use time module to get the current hour.
+ Here is a sample program and documentation link for you:
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+
+
+## Solution:
+
     
 '''
 
 import time 
+
+timestamp = time.strftime('%H:%M:%S ')
+
+print(timestamp)
+
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
 
 
    
