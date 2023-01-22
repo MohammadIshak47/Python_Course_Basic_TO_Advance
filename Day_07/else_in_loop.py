bc5ce814279a484b91816dbd7288b2abf7a958d1
Mohammad <ishak1998bd@gmail.com>
2023-01-22 14:16:36 +0530

    added new files

---
+++ Day_07/else_in_loop.py
@@ -0,0 +1,58 @@
+'''
+Python - else in Loop
+
+As you have learned before, the else clause is used along with the if statement.
+
+Python allows the else keyword to be used with the for and while loops too. The else block appears after the body of the loop. The statements in the else block will be executed after all iterations are completed. The program exits the loop only after the else block is executed.
+Syntax
+
+for counter in sequence:
+    #Statements inside for loop block
+else:
+    #Statements inside else block
+
+Example:
+
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
+
+Output:
+
+iteration no 1 in for loop
+iteration no 2 in for loop
+iteration no 3 in for loop
+iteration no 4 in for loop
+iteration no 5 in for loop
+else block in loop
+Out of loop
+
+'''
+
+# for i in range(1,10):
+#     print(i)
+# else:
+#     print("There is no i")    
+
+
+# for i in []:
+#     print(i)
+# else:
+#     print("No value exists here")    
+
+for i in range(6):
+    print(i)
+    if i==3:
+        break
+else:
+    print("No value here")   ## else will not be printed  
+
+i = 0
+while i<7:
+
+    if i == 4:
+        break
+else:
+    print("Sorry No  i")        
