d38a93157eeb2a555a364aa4f8b5dea379ef47c6
Mohammad <ishak1998bd@gmail.com>
2023-01-22 13:37:05 +0530

    added

--- Day_07/dictionary_methods.py
+++ Day_07/dictionary_methods.py
@@ -62,6 +62,124 @@ Output:
 
 dict_items([('name', 'Karan'), ('age', 19), ('eligible', True)])
 
+
+Dictionary Methods
+
+Dictionary uses several built-in methods for manipulation.They are listed below
+update()
+
+The update() method updates the value of the key provided to it if the item already exists in the dictionary, else it creates a new key-value pair.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)
+info.update({'age':20})
+info.update({'DOB':2001})
+print(info)
+
+Output:
+
+{'name': 'Karan', 'age': 19, 'eligible': True}
+{'name': 'Karan', 'age': 20, 'eligible': True, 'DOB': 2001}
+
+Removing items from dictionary:
+
+There are a few methods that we can use to remove items from dictionary.
+clear():
+
+The clear() method removes all the items from the list.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+info.clear()
+print(info)
+
+Output:
+
+{}
+
+pop():
+
+The pop() method removes the key-value pair whose key is passed as a parameter.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+info.pop('eligible')
+print(info)
+
+Output:
+
+{'name': 'Karan', 'age': 19}
+
+popitem():
+
+The popitem() method removes the last key-value pair from the dictionary.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+info.popitem()
+print(info)
+
+Output:
+
+{'name': 'Karan', 'age': 19, 'eligible': True}
+
+del:
+
+we can also use the del keyword to remove a dictionary item.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info['age']
+print(info)
+
+Output:
+
+{'name': 'Karan', 'eligible': True, 'DOB': 2003}
+
+If key is not provided, then the del keyword will delete the dictionary entirely.
+Example:
+
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info
+print(info)
+
+Output:
+
+NameError: name 'info' is not defined
+
 '''
 
-dic = {'language':'bangla','gender':'male'}
+# dic = {'language':'bangla','gender':'male','age':25}
+
+# print(dic.keys())
+# print(dic.values())
+# print(dic.items())
+# print(dic['language'])
+# print(dic.get('gender'))
+
+
+dic_items = {1:33,2:55,3:99}
+# dic_items2 = {4:88}
+# dic_items.update(dic_items2)
+# print(dic_items)
+
+# dic_items.clear()
+# print(dic_items)
+
+# del dic_items
+# print(dic_items)
+
+
+# del dic_items[2]
+# print(dic_items)
+
+# dic_items.pop(1)
+# print(dic_items)
+
+
+dic_items.popitem()
+print(dic_items)
+
+
+
