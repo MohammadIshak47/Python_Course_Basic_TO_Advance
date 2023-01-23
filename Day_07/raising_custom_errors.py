2dfe122ed6f3ac5ec08a43434bf03971d1c1f641
Mohammad <ishak1998bd@gmail.com>
2023-01-23 10:55:56 +0530

    added new game file

--- Day_07/raising_custom_errors.py
+++ Day_07/raising_custom_errors.py
@@ -33,3 +33,25 @@ exception is raised. For example, sending an error report to the admin, calling 
 
 '''
 
+
+
+# num = int(input("Enter your value between 10 and 20 : "))
+# print(num)
+# if (num<10 or num>20):
+#     raise ValueError("Invalid input. Please enter number between 10 and 20.")
+
+
+## Defining custom exceptions
+
+class CustomError(Exception):
+    def __init__(self,fname,laname):
+        self.fname = fname
+        self.laname = laname
+    def full_name(self):
+        return (f"Your full name is {self.fname}{self.laname}")
+try:
+    name = CustomError('Mohammad', ' Ishak')
+    print(name.full_name())
+except Exception:
+    print('Invalid input.You are entered number. Please enter only string')               
+
