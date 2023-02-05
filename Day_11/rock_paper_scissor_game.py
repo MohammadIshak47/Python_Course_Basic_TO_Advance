faee1a17423500a99abd7761b3c42f6bbb08c4f0
Mohammad <ishak1998bd@gmail.com>
2023-02-05 14:42:58 +0530

    added new solution

--- Day_11/rock_paper_scissor_game.py
+++ Day_11/rock_paper_scissor_game.py
@@ -11,6 +11,29 @@ Use proper functions to check for win.
 
 
 import random
+def check(computer,user):
+    if computer == user:
+        return 0 
+    if computer == 0 and user == 1:
+        return -1 
+    if computer == 1 and user == 2:
+        return -1 
+    if computer == 2 and user == 0:
+        return -1 
+    return 1                 
 computer = random.randint(0, 2)
-user = int(input("Choose Snake ,Water or Gun. 0 for Snake ,1 for Water,-1 for Gun: "))
+user = int(input("Choose Snake ,Water or Gun. 0 for Snake ,1 for Water,2 for Gun: "))
+
+score = check(computer, user)
+
+print("Your taken value :",user)
+print("Computer taken value : ",computer)
+
+if score == 0:
+    print("It's draw")
+elif score == -1:
+    print("You lose")
+else:
+    print("Congrats,You win")    
+
 
